6f1028cfe3820f9e8b760b751671d0c9a827d96d
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-11-22 15:17:50 +0100

    Remove explicit return value + Use 'FT' macro as the subject

--- C/Grace.c
+++ C/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#define s "#include <stdio.h>%c#define s %c%s%c%c#define GRACE() int main() { FILE *fp = fopen(%cGrace_kid.c%c, %cw%c); if (fp == NULL) return 1; fprintf(fp, s, 10, 34, s, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10); fclose(fp); return 0; }%c/*%c    Nowhere comment%c*/%cGRACE()"
-#define GRACE() int main() { FILE *fp = fopen("Grace_kid.c", "w"); if (fp == NULL) return 1; fprintf(fp, s, 10, 34, s, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10); fclose(fp); return 0; }
+#define s "#include <stdio.h>%c#define s %c%s%c%c#define FT() int main() { FILE *fp = fopen(%cGrace_kid.c%c, %cw%c); if (fp == NULL) return 1; fprintf(fp, s, 10, 34, s, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10); fclose(fp); }%c/*%c    Nowhere comment%c*/%cFT()"
+#define FT() int main() { FILE *fp = fopen("Grace_kid.c", "w"); if (fp == NULL) return 1; fprintf(fp, s, 10, 34, s, 34, 10, 34, 34, 34, 34, 10, 10, 10, 10); fclose(fp); }
 /*
     Nowhere comment
 */
-GRACE()
+FT()
